9c3508cd38dc0cf092a44a0491be94d7f8038c10
Alex Zhang <zhangchi0104@live.com>
2024-06-09 23:03:47 +1000

    refactor(auth): migrated auth from auth0 to clerk

--- src/app/(app)/_header/index.tsx
+++ src/app/(app)/_header/index.tsx
@@ -1,31 +1,29 @@
 /** @format */
-import clsx from 'clsx';
-import HeaderMenu from './HeaderMenu';
-import SignInOut from './SignInOut';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import ModeToggle from './DarkModeButton';
-import MobileHeaderMenu from './MobileHeaderMenu';
+import clsx from "clsx";
+import HeaderMenu from "./HeaderMenu";
+import SignInOut from "./SignInOut";
+import ModeToggle from "./DarkModeButton";
+import MobileHeaderMenu from "./MobileHeaderMenu";
 const headerContainerStyle = clsx(
-  'absolute',
-  'w-full',
-  'foreground',
-  'px-4',
-  'pt-2',
-  'flex',
-  'items-center',
-  'justify-between'
+  "absolute",
+  "w-full",
+  "foreground",
+  "px-4",
+  "pt-2",
+  "flex",
+  "items-center",
+  "justify-between",
 );
 const Header = () => {
   return (
-    <header id='header' className={headerContainerStyle}>
-      <div className='md:hidden'>
-        <MobileHeaderMenu className='md:hidden' />
+    <header id="header" className={headerContainerStyle}>
+      <div className="md:hidden">
+        <MobileHeaderMenu className="md:hidden" />
       </div>
-      <HeaderMenu className='items-center justify-start flex-row hidden md:flex' />
-      <div className='flex'>
-        <ModeToggle className='mr-4' />
-        <SignInOut />
+      <HeaderMenu className="items-center justify-start flex-row hidden md:flex" />
+      <div className="flex items-center">
+        <ModeToggle className="mr-4" />
+        <SignInOut className="w-5" />
       </div>
     </header>
   );
